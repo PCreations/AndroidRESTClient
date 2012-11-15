3ed0114812cead2fd5dc548c370ca0834b212445
Pierre <pcriulan@gmail.com>
2012-11-15 18:27:58 +0100

    First commit

---
+++ RestService.java
@@ -0,0 +1,19 @@
+package com.pcreations.restclient;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class RestService extends IntentService{
+
+	public RestService(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
