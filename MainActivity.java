a2ec3365c1953f5d0de14fc5215a01efe0497b11
Pierre <pcriulan@gmail.com>
2012-11-16 10:19:52 +0100

    WebService ok

--- MainActivity.java
+++ MainActivity.java
@@ -18,14 +18,9 @@ public class MainActivity extends Activity implements RestResultReceiver.Receive
         setContentView(R.layout.activity_main);
         mReceiver = new RestResultReceiver(new Handler());
         mReceiver.setReceiver(this);
-        //WebService ws = new WebService(this, mReceiver, "https://maps.googleapis.com/maps/api/place/details/json?reference=CmRYAAAAciqGsTRX1mXRvuXSH2ErwW-jCINE1aLiwP64MCWDN5vkXvXoQGPKldMfmdGyqWSpm7BEYCgDm-iv7Kc2PF7QA7brMAwBbAcqMr5i1f4PwTpaovIZjysCEZTry8Ez30wpEhCNCXpynextCld2EBsDkRKsGhSLayuRyFsex6JA6NPh9dyupoTH3g&sensor=true&key=AIzaSyAir6DKOMRfmXyHgXGHgHSg_FovG7yuMYY");
+        WebService ws = new WebService(this, mReceiver, "http://chupee.fr/json/countries.json");
         Log.i("onCreate", "ws.get()");
-        Intent intent = new Intent(MainActivity.this, RestService.class);
-        intent.setData(Uri.parse("http://chupee.fr/json/countries.json"));
-        intent.putExtra(WebService.METHOD_KEY, WebService.GET);
-        intent.putExtra(WebService.RECEIVER_KEY, mReceiver);
-        this.startService(intent);
-        //ws.get();
+        ws.get();
     }
 
 	public void onReceiveResult(int resultCode, Bundle resultData) {
