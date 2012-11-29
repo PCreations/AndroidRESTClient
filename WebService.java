0c4b1f19ae8d359e66cec213a41f0c0cbaa9a0c3
Pierre <pcriulan@gmail.com>
2012-11-29 17:36:02 +0100

    Clean WebService

--- WebService.java
+++ WebService.java
@@ -6,7 +6,6 @@ import java.io.InputStream;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.ResultReceiver;
 
 public class WebService {
 
