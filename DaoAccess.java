93062eed6e8e38a1b6a9efc38cd6294d9cf8434f
Pierre <pcriulan@gmail.com>
2013-01-23 18:21:41 +0100

    DaoAccess interface is not parametized anymore

--- DaoAccess.java
+++ DaoAccess.java
@@ -3,12 +3,12 @@ package com.pcreations.restclient;
 import java.sql.SQLException;
 import java.util.List;
 
-public interface DaoAccess<R extends ResourceRepresentation<?>> {
+public interface DaoAccess {
 
-	abstract public void updateOrCreate(R resource)throws SQLException;
-	abstract public R findByName(String string) throws SQLException;
-	abstract public R findById(int resourceId) throws SQLException;
-	abstract public R findByNameAndId(String name, int resourceID) throws SQLException;
-	abstract public List<R> queryForAll() throws SQLException;
+	abstract public void updateOrCreate(ResourceRepresentation<?> resource)throws SQLException;
+	abstract public ResourceRepresentation<?> findByName(String string) throws SQLException;
+	abstract public ResourceRepresentation<?> findById(int resourceId) throws SQLException;
+	abstract public ResourceRepresentation<?> findByNameAndId(String name, int resourceID) throws SQLException;
+	abstract public List<ResourceRepresentation<?>> queryForAll() throws SQLException;
 	
 }
