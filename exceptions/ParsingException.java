65e292e4397a9a46f0b6540e2efad2ef4c80138f
Pierre <pcriulan@gmail.com>
2013-01-28 10:04:37 +0100

    Add parsing exception

---
+++ exceptions/ParsingException.java
@@ -0,0 +1,18 @@
+package com.pcreations.restclient.exceptions;
+
+import android.util.Log;
+
+import com.pcreations.restclient.RestService;
+
+public class ParsingException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7301793342104343843L;
+
+	public ParsingException() {
+		Log.e(RestService.TAG, "Parsing exception");
+	}
+	
+}
